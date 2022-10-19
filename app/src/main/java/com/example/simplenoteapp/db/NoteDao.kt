03b3f082d7b488cb823cd75d0cd88f1b3846c121
Barış <barisozkaya3@gmail.com>
2022-10-19 12:03:16 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ app/src/main/java/com/example/simplenoteapp/db/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.example.simplenoteapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.simplenoteapp.model.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query(value = "SELECT * FROM notes ORDER BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
