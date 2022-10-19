1baa673a7d18e73739dce12523ea3077753f7d63
Barış <barisozkaya3@gmail.com>
2022-10-19 14:45:05 +0300

    Fix Layout Design,Fragment

---
+++ app/src/main/java/com/example/simplenoteapp/fragments/HomeFragment.kt
@@ -0,0 +1,52 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.simplenoteapp.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.simplenoteapp.R
+import com.example.simplenoteapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private  var _binding:FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAddNote.setOnClickListener{ mView->
+        mView.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home_menu,menu)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/simplenoteapp/fragments/NewNoteFragment.kt
@@ -0,0 +1,45 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.simplenoteapp.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.example.simplenoteapp.R
+import com.example.simplenoteapp.databinding.FragmentHomeBinding
+import com.example.simplenoteapp.databinding.FragmentNewNoteBinding
+
+
+class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
+
+    private var _binding: FragmentNewNoteBinding? = null
+    private  val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewNoteBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.new_note_menu,menu)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
