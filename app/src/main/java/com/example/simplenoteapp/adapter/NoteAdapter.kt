6fe8ff8c81b02ce9b1fda5b531b18d6fbeaea5d1
Barış <barisozkaya3@gmail.com>
2022-10-23 12:53:55 +0300

    Add NoteAdapter

---
+++ app/src/main/java/com/example/simplenoteapp/adapter/NoteAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.simplenoteapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.AsyncListUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.simplenoteapp.databinding.NoteLayoutAdapterBinding
+import com.example.simplenoteapp.model.Note
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>(){
+
+    private var binding: NoteLayoutAdapterBinding? = null
+
+    class NoteViewHolder(itemBinding: NoteLayoutAdapterBinding): RecyclerView.ViewHolder(itemBinding.root)
+
+    private  val differCallBack =
+        object : DiffUtil.ItemCallback<Note>(){
+            override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    val differ = AsyncListDiffer(this,differCallBack)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        binding = NoteLayoutAdapterBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return  NoteViewHolder(binding!!)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = differ.currentList[position]
+
+        holder.itemView.apply {
+
+            binding?.tvNoteTittle?.text = currentNote.nateTitle
+            binding?.tvNoteBody?.text = currentNote.noteBody
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+      return differ.currentList.size
+    }
+}
